32687537c89efe3bfd9506db5b65489bcf01e3bd
Andrew Dobson <tandrewlopez@gmail.com>
2023-03-30 02:14:16 -0400

    added types to queries

--- store/services/builds.ts
+++ store/services/builds.ts
@@ -1,3 +1,4 @@
+import { GetAllBuildsResponse } from "./../../types/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // IMPORT SMITE SOURCE RESPONSE DATA
 
@@ -6,7 +7,7 @@ export const buildApi = createApi({
   tagTypes: ["Builds"],
   baseQuery: fetchBaseQuery({ baseUrl: "" }),
   endpoints: (builder) => ({
-    getAllBuilds: builder.query<void, void>({
+    getAllBuilds: builder.query<GetAllBuildsResponse[], void>({
       query: () => "https://smitesource.com/api/get-all-builds",
       providesTags: ["Builds"],
     }),
--- store/services/gods.ts
+++ store/services/gods.ts
@@ -1,3 +1,4 @@
+import { GetAllBuildsResponse } from "./../../types/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // IMPORT SMITE SOURCE RESPONSE DATA
 
@@ -6,7 +7,7 @@ export const godsApi = createApi({
   tagTypes: ["Gods"],
   baseQuery: fetchBaseQuery({ baseUrl: "" }),
   endpoints: (builder) => ({
-    getAllGods: builder.query<void, void>({
+    getAllGods: builder.query<GetAllBuildsResponse[], void>({
       query: () => "https://smitesource.com/api/get-all-gods",
       providesTags: ["Gods"],
     }),
